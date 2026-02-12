eb2a0af43d8d40d4c10482c0eb3923dbdb7832e7
bPraet <25182644+bPraet@users.noreply.github.com>
2026-02-12 15:46:10 +0100

    Update runAsSystemWingetInstall.ps1

--- runAsSystemWingetInstall.ps1
+++ runAsSystemWingetInstall.ps1
@@ -1,5 +1,37 @@
+#Installation via winget système
+
 $laptop = Read-SpectreText -Message "Target: "
 $packageID = Read-SpectreText -Message "Winget package ID: "
+
+#Check online
+if (Test-Connection $laptop -Count 1 -ea SilentlyContinue -wa SilentlyContinue) {
+    Write-Host "`n$laptop is online" -ForegroundColor Green
+}
+else {
+    Write-Host "`n$laptop is not online" -ForegroundColor Red
+    Read-Host -Prompt "Press Enter to quit" | Out-Null
+    exit
+}
+
+#Clean
+if (Test-Path \\$laptop\c$\temp\execNow.ps1) {
+    Remove-Item \\$laptop\c$\temp\execNow.ps1
+}
+else {
+    Write-Host "No script"
+}
+
+Invoke-Command -ComputerName $laptop -ScriptBlock {
+    Get-ScheduledTask -TaskName "execNow" -ErrorAction SilentlyContinue -OutVariable execNowTask
+
+    if ($execNowTask) {
+        Unregister-ScheduledTask -TaskName "execNow" -Confirm:$false
+    }
+    else {
+        Write-Host "No task"
+    }
+}
+
 #Push script to remote target
 #Ex path winget: 'C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.26.430.0_x64__8wekyb3d8bbwe\winget.exe'
 @"
@@ -14,6 +46,8 @@ Invoke-Command -ComputerName $laptop -ScriptBlock {
 
     Register-ScheduledTask -TaskName "execNow" -User "System" -Action $task -Settings $settings
     Start-ScheduledTask -TaskName "execNow"
-
 }
 
+Write-Host "Installation is running, logs are in C:\temp\execNow.log on target computer" -NoNewLine
+$Host.UI.ReadLine()
+exit
